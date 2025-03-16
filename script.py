00ba1bcd7a45e224747f88b83622aa48c843c454
RazvanDiaconescu <119290855+Razvanlog@users.noreply.github.com>
2025-03-16 16:39:56 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -10,128 +10,66 @@ import shutil
 import numpy as np
 from PIL import Image
 db_filename='/home/razvan/repos/itfest2025_AImodeltraining/BrainTrace_Turtles_ITfest2025_site/demo/db.sqlite3'
+photo_storage='/home/razvan/repos/itfest2025_AImodeltraining/BrainTrace_Turtles_ITfest2025_site/demo/media/'
+# '/tumor_images/'
 ok=1
 if ok==1:
     conn=sqlite3.connect(db_filename)
     cursor=conn.cursor()
-    cursor.execute('SELECT  FROM myapp_patientdata')
+    cursor.execute('SELECT first_name,last_name,email,tumor_image FROM myapp_patientdata')
     conn.commit()
     rows=cursor.fetchall()
-    
-    
     #WORKS
     model=tf.keras.models.load_model('brain_tumor_version2.keras')
-    photo_location='archive/Testing/notumor/Te-no_0010.jpg'
-    image_string=tf.io.read_file('/home/razvan/repos/itfest2025_AImodeltraining/'+photo_location)
-    image=tf.image.decode_image(image_string,channels=3)
-    target_size=(128,128)
-    image=tf.image.resize(image,target_size)
-        # image=tf.image.convert_image_dtype(image,tf.float32)
-        # image=(image-127.5)/127.5
-        # result=model.predict(image)
-        # print(image.shape)
-    result=-1
-    if image.shape==(128,128,3):
-        image=tf.expand_dims(image,axis=0)
-            # dataset=tf.data.Dataset.from_tensor_slices(image)
-            # dataset=dataset.batch(1)
-            # print(dataset)
-        result=model(image)
-        print(np.argmax(result,axis=1))
-    print(result,'succes')
-    arr=['Glioma','Meningioma','No tumor','Pituitary']
-    pos=0
-    strResult=arr[np.argmax(result,axis=1)[0]]
-
-
-
-    # print(strResult)
-    # for i in list(result):
-    #     if i==True:
-    #         strResult=arr[pos]
-    #     pos+=1
-    # strResult=arr[tf.where(tf.equal(result,1))]
-
-    # print('succes')
-    for i in rows:
-        (number,photo_location,f_name,l_name,receiver_email,age)=i
-        ok=0
-        im=Image.open('/home/razvan/repos/isic_2024/web_site/isic/media'+photo_location)
-        width,height=im.size
-        fwidth,fheight=(128,128)
-        left=width//2-fwidth//2
-        right=width//2+fwidth//2
-        top=height//2-fheight//2
-        bottom=height//2+fheight//2
-        im=im.crop((left,top,right,bottom))
-        im.save('/home/razvan/repos/isic_2024/web_site/isic/media'+photo_location)
-        photo_location='archive/Testing/notumor/Te-no_0010.jpg'
-        # image_string=tf.io.read_file('/home/razvan/repos/itfest2025_AImodeltraining/'+photo_location)
-        # image=tf.image.decode_image(image_string,channels=3)
-        # target_size=(128,128)
-    #     image=tf.image.resize(image,target_size)
-    #     # image=tf.image.convert_image_dtype(image,tf.float32)
-    #     # image=(image-127.5)/127.5
-    #     # result=model.predict(image)
-    #     # print(image.shape)
-    #     result=-1
-    #     if image.shape==(128,128,3):
-    #         image=tf.expand_dims(image,axis=0)
-    #         # dataset=tf.data.Dataset.from_tensor_slices(image)
-    #         # dataset=dataset.batch(1)
-    #         # print(dataset)
-    #         result=model(image)
-    #         print(np.argmax(result,axis=1))
-    #     print(result,'succes')
-    #     # if ok==1:
-    #     #     smtp_server='smtp.gmail.com'
-    #     #     port=587
-        #     sender_email='webemailt@gmail.com'
-        #     message=MIMEText('Hello World!')
-        #     context=ssl.create_default_context()
-        #     print('ok')
-        #     with smtplib.SMTP(smtp_server,port) as server:
-        #         print('ok')
-        #         server.ehlo()
-        #         print('ok')
-        #         server.starttls(context=context)
-        #         server.ehlo()
-        #         print('ok')
-        #         server.login(sender_email,password)
-        #         server.sendmail(sender_email,receiver_email,message.as_string())
-        #         print("succes")
-# # 'csim18782@gmail.com'
-
-        # print(type(i))
-# except:
-#     print('cannot connect to database')
-
-# print(rows)
-
-
     smtp_server='smtp.gmail.com'
     port=587
     sender_email='braintrace.turtles@gmail.com'
-    # 'csim18782@gmail.com'
-    receiver_email='razvan.diaconescu04@e-uvt.ro'
-    msg=MIMEMultipart()
-    from email.header import Header
-    msg['subject']=Header('Your result to the MRI analysis')
-    msg['From']=sender_email
-    msg['To']=receiver_email
-    message=MIMEText('Hello User!\n The result of the analysis of the MRI scan is '+strResult+'.')
-    msg.attach(message)
-   
-    context=ssl.create_default_context()
-    print('ok')
-    with smtplib.SMTP(smtp_server,port) as server:
-        print('ok')
-        server.ehlo()
-        print('ok')
-        server.starttls(context=context)
-        server.ehlo()
-        # print('ok')
-        server.login(sender_email,password)
-        # server.sendmail(sender_email,receiver_email,msg)
-        server.send_message(msg)
-        print("succes")
+    for i in rows:
+        (fname,lname,email,photo_location)=i
+        ok=0
+        if photo_location!="" or photo_location!=None:
+            # print(photo_storage+" "+photo_location)
+            try:
+                im=Image.open(photo_storage+photo_location)
+                width,height=im.size
+                fwidth,fheight=(128,128)
+                left=width//2-fwidth//2
+                right=width//2+fwidth//2
+                top=height//2-fheight//2
+                bottom=height//2+fheight//2
+                im=im.crop((left,top,right,bottom))
+                im.save(photo_storage+photo_location)
+                im_string=tf.io.read_file(photo_storage+photo_location)
+                img=tf.image.decode_image(im_string,channels=3)
+                target_size=(128,128)
+                img=tf.image.resize(img,target_size)
+                result=-1
+                if img.shape==(128,128,3):
+                    img=tf.expand_dims(img,axis=0)
+                    result=model(img)
+                print('succes')
+                arr=['Glioma','Meningioma','No tumor','Pituitary']
+                strResult=arr[np.argmax(result,axis=1)[0]]
+                print(strResult)
+                receiver_email=email
+                msg=MIMEMultipart()
+                from email.header import Header
+                msg['subject']=Header('Your result to the MRI analysis')
+                msg['From']=sender_email
+                msg['To']=receiver_email
+                message=MIMEText('Hey '+fname+' '+lname+',\n The result of the analysis of the MRI scan is '+strResult+'.')
+                msg.attach(message)
+                password='asxyyoywyunwpedh'
+                context=ssl.create_default_context()
+                print('ok')
+                with smtplib.SMTP(smtp_server,port) as server:
+                    print('ok')
+                    server.ehlo()
+                    print('ok')
+                    server.starttls(context=context)
+                    server.ehlo()
+                    server.login(sender_email,password)
+                    server.send_message(msg)
+                    print("succes"+" "+receiver_email)
+            except:
+                a=0
